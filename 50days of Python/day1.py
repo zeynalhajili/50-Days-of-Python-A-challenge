128a85975ef2a8d39318e1629b3b88029dbb145a
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2023-01-23 00:58:47 +0100

    23.01.2023

--- 50days of Python/day1.py
+++ 50days of Python/day1.py
@@ -3,7 +3,16 @@ Write a function called divide_or_square that takes one argument (a number), and
 returns its remainder if it is not divisible by 5. For example, if you pass 10 as an argument, then your function should return 3.16 as the square root.
 """
 
-def divide_or_square(number):
-    return "hell1o world"
+from math import sqrt
 
-print(divide_or_square(5))
+
+def divide_or_square(argument):
+    if argument % 5 == 0:
+        return f'{sqrt(argument):.2f}'
+    else:
+        return argument % 5
+    
+print(divide_or_square(10))
+print(divide_or_square(9))
+print(divide_or_square(8))
+print(divide_or_square(7))
