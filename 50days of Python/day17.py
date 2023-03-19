1d1144feaa5a29969dea96af6c361e9923de82dd
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2023-03-19 11:11:02 +0100

    update for 19.03.2023

---
+++ 50days of Python/day17.py
@@ -0,0 +1,15 @@
+"""
+Day 17: User Name Generator
+Write a function called user_name, that creates a username for the user. The function
+should ask a user to input their name. The function should then reverse the name and 
+attach a randomly issued number between 0 – 9 at the end of the name. The function 
+should return the username.
+"""
+import random 
+
+def user_name(input_name):
+    random_number =random.randint(0,9)
+    return  input_name[::-1] + str(random_number)
+
+print(user_name("Zeynal"))
+
