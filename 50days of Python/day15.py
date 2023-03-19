84de1331b0946a36dd888faa543a8b743dce50ee
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2023-03-19 09:38:24 +0100

    update for 19.03.2023

---
+++ 50days of Python/day15.py
@@ -0,0 +1,15 @@
+"""
+Day 15: Same in Reverse
+Write a function called same_in_reverse that takes a string and checks if the string
+reads the same in reverse. If it is the same, the code should return True if not, it 
+should return False. For example, ‘dad’ should return True, because it reads the same
+in reverse.
+"""
+def same_in_reverse(user_input):
+    if user_input[::-1] == user_input[::]:
+        return True
+    else:
+        return False
+
+print(same_in_reverse("dad"))
+print(same_in_reverse("dad11"))
