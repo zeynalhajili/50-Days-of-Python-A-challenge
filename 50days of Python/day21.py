46566d96c1dfbaef2e74ebc8766398456e5656c7
Zeynal Hajili <38220178+zhajili91@users.noreply.github.com>
2023-03-19 18:58:48 +0100

    update for 19.03.2023

--- 50days of Python/day21.py
+++
@@ -1,9 +0,0 @@
-"""
-Day 21: List of Tuples
-Write a function called make_tuples that takes two lists, equal lists, and combines 
-them into a list of tuples. For example if list a is [1,2,3,4] and list b is 
-[5,6,7,8], your function should return [(1,5),(2,6),(3,7),(4,8)].
-"""
-
-def make_tuples(list_a,list_b):
-    
